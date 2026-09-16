79c4bdbbf9aeed4f2f064a80efd1ac5e4eceecb9
alikhan zhambayev <254530@astanait.edu.kz>
2026-09-16 21:49:35 +0500

    feature quicksort add quick sort

---
+++ src/main/java/org/example/QuickSort.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.util.Random;
+public class QuickSort {
+    private static final Random random = new Random();
+
+    public static void sort (int[] arr, Metrics metrics){
+        if(arr == null || arr.length <= 1){
+            return;
+        }
+
+        metrics.startTimer();
+        quickSort(arr,0,arr.length - 1, 1, metrics);
+        metrics.stopTimer();
+    }
+
+    private static void quickSort(int[] arr, int left, int right, int depth, Metrics metrics){
+        metrics.updateDepth(depth);
+
+        while (left < right){
+            int[] pivots = partition(arr, left, right,metrics);
+            int lt = pivots[0];
+            int gt = pivots[1];
+
+            if (lt - left < right - gt){
+                quickSort(arr,left,lt,depth+1, metrics);
+                left = gt;
+            }
+            else{
+                quickSort(arr,gt,right,depth+1, metrics);
+                right = lt;
+            }
+        }
+    }
+
+    private static int[] partition(int[] arr, int left, int right, Metrics metrics) {
+        int pivotIndex = left + random.nextInt(right - left + 1);
+        swap(arr, left, pivotIndex);
+        int pivot = arr[left];
+
+        int lt = left;
+        int gt = right;
+        int i = left + 1;
+
+        while (i <= gt) {
+            metrics.incrementComparisons();
+            if (arr[i] < pivot) {
+                swap(arr, lt++, i++);
+            } else {
+                metrics.incrementComparisons();
+                if (arr[i] > pivot) {
+                    swap(arr, i, gt--);
+                } else {
+                    i++;
+                }
+            }
+
+        }
+        return new int[]{lt - 1, gt + 1};
+    }
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+
+
+}
